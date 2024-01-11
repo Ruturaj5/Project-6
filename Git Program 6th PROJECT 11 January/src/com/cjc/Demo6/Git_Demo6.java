e39b38bef0949e16c3af2c3e2be935b536359753
rutur <rutur@RUTURAJ>
2024-01-11 16:36:11 +0530

    By Ruturaj

---
+++ Git Program 6th PROJECT 11 January/src/com/cjc/Demo6/Git_Demo6.java
@@ -0,0 +1,19 @@
+package com.cjc.Demo6;
+
+public class Git_Demo6 
+{
+
+	public void m6() 
+
+	{
+		System.out.println( "method m6");
+
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Main method");
+		Git_Demo6  mm = new Git_Demo6 ();
+		mm.m6();
+	}
+
+}
